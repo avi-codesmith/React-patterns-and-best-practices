8864ebfecef116df692a5720b2ba9059de7289c7
avi-codesmith <avi.codesmith@gmail.com>
2026-04-09 18:52:26 +0530

    Compound Components in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import Accordion from "./components/Accordion";
+import AccordionItem from "./components/Accordion-item";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion className="accordion">
+          <AccordionItem title="This is just best!" className="accordion-item">
+            <article>
+              <p>You will be the best by using that!</p>
+              <p>We are in the business which is made for people like you!</p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            title="This is the right place!"
+            className="accordion-item"
+          >
+            <article>
+              <p>You are luckiest animal in this whole world!</p>
+              <p>We are here for you!</p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
